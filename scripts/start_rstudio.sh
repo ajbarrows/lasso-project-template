fcb57cd8e4dbe436dddd7dbdb542c564459d6f2a
Anthony Barrows <ajbarrow@uvm.edu>
2026-03-03 13:24:30 -0500

    add rstudio shell script

---
+++ scripts/start_rstudio.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Usage: ./start_rstudio.sh [port]
+
+PORT=${1:-8888}
+SIF="$(dirname "$0")/../project-container.sif"
+WORKDIR=$(mktemp -d)
+
+echo "Starting RStudio Server on port $PORT"
+echo "Open browser at: http://localhost:$PORT"
+echo "Username: $(whoami)"
+echo "Password: Set PASSWORD env variable (default: rstudio)"
+echo ""
+
+PASSWORD=${PASSWORD:-rstudio}
+
+singularity exec \
+    --scratch /run,/var/lib/rstudio-server \
+    --workdir "$WORKDIR" \
+    --bind "$(pwd):/project" \
+    --env PASSWORD=$PASSWORD \
+    --env RS_INITIAL_PROJECT=/project/project.Rproj \
+    "$SIF" \
+    rserver \
+        --www-address=0.0.0.0 \
+        --www-port=$PORT \
+        --server-user=$(whoami) \
+        --auth-none=0 \
+        --auth-pam-helper-path=pam-helper
