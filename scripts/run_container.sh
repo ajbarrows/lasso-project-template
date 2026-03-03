686f3855535dfafae6e655be61e9f4b7c0923218
Anthony Barrows <ajbarrow@uvm.edu>
2026-03-03 15:53:19 -0500

    mess with symlink mapping

--- scripts/run_container.sh
+++ scripts/run_container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # run_container.sh
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
-apptainer shell --bind /shared:${SCRIPT_DIR}/data/raw "$SCRIPT_DIR/project-container.sif"
+apptainer shell --bind /shared:/data/raw "$SCRIPT_DIR/../project-container.sif"
